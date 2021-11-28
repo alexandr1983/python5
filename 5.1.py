d49940c40c597c880e2b406fb53ec1f5582a3b21
alexandr1983 <alexandr.mullin@gmail.com>
2021-11-28 11:52:30 +0300

    5.1.py

---
+++ 5.1.py
@@ -0,0 +1,13 @@
+text_file = open('test51.txt', 'w')
+line = input('Enter text: \n')
+while line:
+    text_file.writelines(line)
+    line = input('Enter text: \n')
+    if not line:
+        break
+
+text_file.close()
+text_file = open('test51.txt', 'r')
+content = text_file.readlines()
+print(content)
+text_file.close()
